7bffa6f0abc21b236b53656a7beca1db09c4b62d
kaliprasad1998 <kancharlaguruprasad@gmail.com>
2020-08-11 21:24:54 +0530

    created some custom widgets

---
+++ lib/icon_content.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class IconContent extends StatelessWidget {
+  IconContent({@required this.myIcon, @required this.myText});
+  final IconData myIcon;
+  final String myText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          myIcon,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          myText,
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Color(0xFF8D8E98),
+          ),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/input_page.dart
@@ -0,0 +1,64 @@
+import 'package:bmi_calculator/reusable_card.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_content.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    colour: Color(0xFF1D1E33),
+                    cardChild: IconContent(
+                      myIcon: FontAwesomeIcons.mars,
+                      myText: 'MALE',
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    cardChild: IconContent(
+                        myIcon: FontAwesomeIcons.venus, myText: 'FEMALE'),
+                    colour: Color(0xFF1D1E33),
+                  ),
+                ),
+              ],
+            )),
+            Expanded(
+              child: ReusableCard(
+                colour: Color(0xFF1D1E33),
+              ),
+            ),
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    colour: Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: Color(0xFF1D1E33),
+                  ),
+                ),
+              ],
+            )),
+          ],
+        ));
+  }
+}
